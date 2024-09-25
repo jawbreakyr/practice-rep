ae25ef07232f4a9266abaa531ac3a068c4759f96
hubert <jawbreakyr@gmail.com>
2024-09-25 22:50:28 +0100

    palying around with views and urls

--- polls/urls.py
+++ polls/urls.py
@@ -4,6 +4,14 @@ from . import views
 
 
 urlpatterns = [
-	path("", views.index, name="index"),
-
+    # ex: /polls/
+	path("polls/", views.index, name="index"),
+    # ex: /1/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /1/results
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /1/vote
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+	# user requests a url which is resolved through this urls.py 
+    # which thens calls the corresponding function inside the views.py 
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Welcome foo-bar")
+	return HttpResponse("Welcome to my new django app.")
+
+def detail(request,question_id):
+	return HttpResponse("Youre looking at question %s" %question_id)
+
+def results(request, question_id):
+	response = "You're looking at results of question %s"
+	return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+	return HttpResponse("You're voting on question %s" % question_id)
