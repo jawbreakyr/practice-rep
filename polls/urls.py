55804b5c535c100a0185f2a5d605237508ea2dda
hubert <jawbreakyr@gmail.com>
2024-09-26 04:07:43 +0100

    playing with urls

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/
-	path("polls/", views.index, name="index"),
+	path("", views.index, name="index"),
     # ex: /1/
     path("<int:question_id>/", views.detail, name="detail"),
     # ex: /1/results
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from django.template import loader
 
-# Create your views here.
+from .models import Question
+
+
+# def index(request):
+# 	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+# 	# output = ", ".join([q.question_text for q in latest_question_list])
+# 	# parse each object with comma
+# 	template = loader.get_template("polls/index.html")
+# 	context = {
+# 		"latest_question_list": latest_question_list,
+# 	}
+# 	return HttpResponse(template.render(context, request))
+
+# shortcut render
 def index(request):
-	return HttpResponse("Welcome to my new django app.")
+	latest_question_list = Question.objects.order_by("-pub_date")[:5]
+	context = {
+		"latest_question_list": latest_question_list,
+	}
+	return render(request, "polls/index.html", context)
 
-def detail(request,question_id):
-	return HttpResponse("Youre looking at question %s" %question_id)
+# def detail(request,question_id):
+# 	try:
+# 		question = Question.objects.get(pk=question_id)
+# 	except Question.DoesNotExist:
+# 		raise Http404("Question Does Not Exist Mother Fucker!!")
+	
+# 	return render(request, "polls/detail.html", {"question": question})
+
+# shortcut render && get_object_or_404
+def detail(request, question_id):
+	question = get_object_or_404(Question, pk=question_id)
+	# for i in dir(question):
+	# 	print(i)
+	return render(request, "polls/detail.html", {"question":question})
 
 def results(request, question_id):
 	response = "You're looking at results of question %s"
